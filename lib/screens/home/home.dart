bbc70176596122a0acbc721b0f7399355533d07e
Aiwar4s <vaitkusaivaras5@gmail.com>
2022-04-05 02:01:19 +0300

    added button

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:untitled/%20services/auth.dart';
 import 'package:untitled/%20services/database.dart';
+import 'package:untitled/main.dart';
 import 'package:untitled/models/bentis.dart';
 import 'package:untitled/screens/home/bentis_list.dart';
 
@@ -18,17 +19,66 @@ class Home extends StatelessWidget {
       showModalBottomSheet(context: context, builder: (context) {
         return Container(
           padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 60.0),
-          child: const Text('botttom sheet'),
-
+          child: const Text('bottom sheet'),
         );
       });
     };
+
+    Widget _showPostCreation(BuildContext context){
+      return AlertDialog(
+        title: const Text('Post Creation'),
+        content: Column(
+          children: const <Widget>[
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Departure',
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Destination'
+                ),
+              ),
+            ),
+          ],
+        ),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () => Navigator.pop(context, 'Cancel'),
+            child: const Text('Cancel'),
+          ),
+          TextButton(
+            onPressed: () => Navigator.pop(context, 'Cancel'),
+            child: const Text('OK'),
+          ),
+        ],
+      );
+    }
 
     return StreamProvider<List<Bentis1>>.value(
       value: DatabaseService(uid:"").Bentis,
       initialData: [],
       child: Scaffold(
       backgroundColor: Colors.brown[50],
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
+          onPressed:()=>
+          {
+            showDialog(
+              context: context,
+              builder: (BuildContext context) => _showPostCreation(context),
+            ),
+          },
+      ),
       appBar: AppBar(
         title: const Text('Bentis'),
         backgroundColor: Colors.brown[400],
@@ -44,7 +94,7 @@ class Home extends StatelessWidget {
           TextButton.icon(
             icon:const Icon(Icons.settings),
             label: const Text('settings'),
-            onPressed:()=> _showSettingsPanel(context),
+            onPressed: () => _showSettingsPanel(context),
           )
         ]
       ),
